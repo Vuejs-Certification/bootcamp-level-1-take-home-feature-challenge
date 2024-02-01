713f5d06a555b3c6f33005bbd77da31e63cfbf2b
Daniel Kelly <me@danielkelly.io>
2024-02-01 12:47:34 -0600

    start with the boilerplate

--- src/components/RatingInput.vue
+++ src/components/RatingInput.vue
@@ -1,57 +1,23 @@
 <script setup>
 import { StarIcon as StarIconOutline } from "@heroicons/vue/24/outline";
 import { StarIcon as StarIconSolid } from "@heroicons/vue/24/solid";
-const props = defineProps({
-  modelValue: { type: Number, default: 0 },
-  count: { type: Number, default: 5 },
-});
-
-const emit = defineEmits({
-  "update:modelValue": (value) => {
-    return typeof value === "number";
-  },
-});
-
-function handleKeyboardShortcuts(e) {
-  const supportedKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  if (supportedKeys.includes(e.key)) {
-    emit("update:modelValue", Number(e.key));
-  }
-  if (
-    ["ArrowRight", "ArrowUp"].includes(e.key) &&
-    props.modelValue < props.count
-  ) {
-    emit("update:modelValue", props.modelValue + 1);
-  }
-  if (["ArrowLeft", "ArrowDown"].includes(e.key) && props.modelValue > 0) {
-    emit("update:modelValue", props.modelValue - 1);
-  }
-}
 </script>
 <template>
-  <div
-    autofocus
-    tabindex="0"
-    @keydown="handleKeyboardShortcuts"
-    class="rating-wrapper"
-    data-test="rating-wrapper"
-    :class="{
-      'perfect-rating': modelValue === count,
-    }"
-  >
-    <button
-      v-for="i in count"
-      tabindex="-1"
-      :key="i"
-      @click="() => $emit('update:modelValue', i)"
-      data-test="rating-button"
-    >
-      <StarIconSolid
-        v-if="i <= modelValue"
-        data-test="solid-star"
-        class="solid-star"
-      />
-      <StarIconOutline v-else data-test="outline-star" class="outline-star" />
+  <div autofocus tabindex="0" data-test="rating-wrapper">
+    <button tabindex="-1" data-test="rating-button">
+      <StarIconSolid data-test="solid-star" class="solid-star" />
+    </button>
+    <button tabindex="-1" data-test="rating-button">
+      <StarIconSolid data-test="solid-star" class="solid-star" />
+    </button>
+    <button tabindex="-1" data-test="rating-button">
+      <StarIconSolid data-test="solid-star" class="solid-star" />
+    </button>
+    <button tabindex="-1" data-test="rating-button">
+      <StarIconOutline data-test="outline-star" class="outline-star" />
+    </button>
+    <button tabindex="-1" data-test="rating-button">
+      <StarIconOutline data-test="outline-star" class="outline-star" />
     </button>
   </div>
 </template>
