dafd9a42117a521e0a2cf33333a40619ddb5d824
Daniel Kelly <me@danielkelly.io>
2024-06-26 13:07:47 -0500

    feature: console logs to terminal

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@/assets/main.css";
+import terminal from "virtual:terminal";
+// eslint-disable-next-line
+globalThis.console = terminal;
 
 createApp(App).mount("#app");
--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,17 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
+import Terminal from "vite-plugin-terminal";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+    Terminal({
+      output: ["terminal", "console"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
